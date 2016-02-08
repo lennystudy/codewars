83842a238d708ddee6e173a99f78857379be539d
lenny <liangnian1155@gmail.com>
2016-02-08 13:09:39 +0800

    这个理解题目有误，理解成最长的那个字符串开始的K个字符了

--- src/codewars/LongestConsec.java
+++ src/codewars/LongestConsec.java
@@ -1,7 +1,5 @@
 package codewars;
 
-import java.util.Arrays;
-
 /**
  * You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
  * lenny
@@ -14,21 +12,34 @@ public class LongestConsec {
     	if(strarr.length==0 || k<=0 || k>strarr.length){
     		return answer;
     	}
-    	int[] strarrLength = new int[strarr.length];
+    	int longStringLength = 0;
+    	int maxIndex = 0;
     	for(int i=0;i<strarr.length;i++){
-    		strarrLength[i] = strarr[i].length();
+    		if(longStringLength < strarr[i].length()){
+    			maxIndex = i;
+    			longStringLength = strarr[i].length();
+    		}
     	}
     	//notice that k still may return the wrong answer,k strings may cross the border
     	//but the problem is that program looks not beautiful
     	//so this method is wrong
-    	Arrays.sort(strarrLength);
-    	if(strarrLength[strarr.length]+k-1 > strarr.length ){
+    	
+    	if(maxIndex+k-1 > strarr.length ){
     		return answer;
     	}
     	StringBuffer stringBuffer = new StringBuffer();
     	for(int i=0;i<k;i++){
-    		stringBuffer.append();
+    		stringBuffer.append(strarr[maxIndex++]);
     	}
+    	answer = stringBuffer.toString();
     	return answer;
     }
+    
+    public static void main(String[] args){
+    	System.out.println("LongestConsec.main()");
+    	String[] strarr = new String[]{"hello","lenny","ivwswynn"};
+    	System.out.println(LongestConsec.longestConsec(strarr, 1));
+    }
+    
+    
 }
--- src/codewars/LongestConsecTest.java
+++ src/codewars/LongestConsecTest.java
@@ -10,6 +10,8 @@ public class LongestConsecTest {
 	public void test() {
 		fail("Not yet implemented");
 		System.out.println("Test start");
+		String[] strings = new String[]{"hello","world","lenny"};
+		LongestConsec.longestConsec(strings, 1);
 	}
 
 }
